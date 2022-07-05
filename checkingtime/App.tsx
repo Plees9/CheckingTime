816a04d47c9440e7b399cdc4e359632ad9de23d1
Lee Gobin <Plees9>
2022-07-05 11:59:27 +0700

    update

--- checkingtime/App.tsx
+++ checkingtime/App.tsx
@@ -1,9 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-<<<<<<< HEAD
 import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
-=======
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
->>>>>>> 8490ebb88ffee333b882e4b6736cf9c8db7103d8
 import { NavigationContainer } from "@react-navigation/native";
 import React from 'react';
 
@@ -34,7 +30,7 @@ const App = () => {
         {/* <NewPasswordScreen/> */}
         {/*<Navigation/> */}
         {/* <SignInScreen/> */}
-        <Navigation/>
+        {/* <Navigation/> */}
       
       </SafeAreaView>
   );
