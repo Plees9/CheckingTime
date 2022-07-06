1ce67a0f7032099ad97865512df8de5337555c0f
Lee Gobin <Plees9>
2022-07-06 15:23:48 +0700

    update

--- checkingtime/src/Screens/HomeScreen/index.tsx
+++ checkingtime/src/Screens/HomeScreen/index.tsx
@@ -11,46 +11,6 @@ import Jobs from "../Jobs";
 const Tab = createBottomTabNavigator();
 const HomeScreen = () => {
     return (
-<<<<<<< HEAD
-        <View style={styles.container}>
-            <ScrollView style={styles.scrollView}>
-                <View style={{flex:1, flexDirection:'row'}}>
-                    <View style={{flex:1, backgroundColor:'lightblue',borderWidth:1, padding: 10,marginVertical:30,marginLeft:40, borderRadius:8}}>
-                        <Text style={styles.text}>Home</Text>
-                    </View>
-                    <View style={{flex:1,marginLeft:4, backgroundColor:'lightblue',borderWidth:1,padding: 10,marginVertical:30,marginRight:40,borderRadius:8}}>
-                        <Text style={styles.text}>Jobs</Text>
-                    </View>
-                </View>
-                <Text>Workspace của bạn</Text>
-                <View style={styles.item}>
-                    <View style={{alignContent : "center", justifyContent: "center"}}>
-                    <Icon name="home" size={30} color="black" />
-                    </View>
-                    <View>
-                        <Text>Home1</Text>
-                        <Text>Home2</Text>
-                    </View>
-                
-                    <Pressable style = {{alignContent :'center', justifyContent : "center",padding: 12,borderRadius:100, backgroundColor: 'lightblue'}}>
-                        <Icon name="reload1" size={20} onPress={() => Alert.alert('Simple Button pressed') }/>
-                    </Pressable>
-                    
-                </View>
-                <Text>Top 5 hôm nay</Text>
-                <View style={{marginHorizontal:20}}>
-                    <View style={styles.item2}>
-                        <Text>Person1</Text>
-                        <Text>Person2</Text>
-                        <Text>Person3</Text>
-                        <Text>Person4</Text>
-                        <Text>Person5</Text>
-                        <Text>Person6</Text>
-                        <Text>Person7</Text>
-                    </View>
-                </View>
-            </ScrollView>
-=======
       <View style={styles.container}>
         <ScrollView style={styles.scrollView}>
           <View style={{ flex: 1, flexDirection: "row" }}>
@@ -124,7 +84,6 @@ const HomeScreen = () => {
         <View>
              
           
->>>>>>> 23ff20b50e3cf98322ae028f360a75a721fbcae2
         </View>
       </View>
     );
