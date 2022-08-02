ac605f2bea6abfc316732a4362c23e055e141b5a
Bakien-1801 <nguyensonba_t64@hus.edu.vn>
2022-08-02 09:47:08 +0700

    date

--- checkingtime/src/Screens/Account/Account.tsx
+++ checkingtime/src/Screens/Account/Account.tsx
@@ -15,11 +15,6 @@ import { useNavigation } from "@react-navigation/native";
 import createStyles from "./styles";
 import { useDispatch, useSelector } from "react-redux";
 import { logout } from "../../../redux/action";
-<<<<<<< HEAD
-=======
-import {Color_Icon} from "../../../constants/theme";
-import { LinearGradient } from "expo-linear-gradient";
->>>>>>> ba71d555e7f1ee4d37a40775f2a1999a905b8e78
 import * as ImagePicker from "expo-image-picker";
 
 const Account = () => {
@@ -50,10 +45,7 @@ const pickImage = async () => {
 };
 const takeImage = async () => {
   // No permissions request is necessary for launching the image library
-<<<<<<< HEAD
   //Permissions.askAsync (Permissions.CAMERA_ROLL)
-=======
->>>>>>> ba71d555e7f1ee4d37a40775f2a1999a905b8e78
   let result = await ImagePicker.launchCameraAsync({
     mediaTypes: ImagePicker.MediaTypeOptions.All,
     allowsEditing: true,
